42e1ca73728d9110d48e75c0d1cdf2f2a17ce211
Janek <eunho.moon@gmail.com>
2023-07-03 22:29:04 +0900

    catalog - Entity 수정

--- catalog/src/main/java/me/janek/catalog/domain/catalog/Catalog.java
+++ catalog/src/main/java/me/janek/catalog/domain/catalog/Catalog.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Catalog implements Serializable {
 
